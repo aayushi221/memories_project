015b1ff3cd1adbfe3092a8e0e2f817f63da6dcb0
aayushi221 <aayushipandey221@gmail.com>
2021-07-23 00:14:32 +0530

    axios and api folder added

--- client/src/components/Posts/styles.js
+++ client/src/components/Posts/styles.js
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
+import useStyles from './styles';
+
 
 export default makeStyles((theme) => ({
+    const classes = useStyles();
   mainContainer: {
     display: 'flex',
     alignItems: 'center',
