d1f3a1799b2ec6d0c25fa976d0cf97333691b264
aayushi221 <aayushipandey221@gmail.com>
2021-08-07 12:03:08 +0530

    form using material ui

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit : "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit : "30mb", extended:true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL ='mongodb+srv://aayushipandey221:8935047120@cluster0.of6we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
