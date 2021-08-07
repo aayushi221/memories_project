ab21528ccc146599053bf83be76e64a1767c67cf
aayushi221 <aayushipandey221@gmail.com>
2021-08-07 12:35:04 +0530

    part 1 complete

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,12 +1,22 @@
 import * as api from '../api';
 
-export const getPosts =() =>  async (dispatch) => {
+export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        
+
         dispatch({ type: 'FECTH_ALL', payload: [] });
     } catch (error) {
         console.log(error.message);
     }
 
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data })
+    } catch (error) {
+        console.log(error);
+    }
 }
