47c7c89da6a10575c81d9bc621791ea3f488970b
aayushi221 <aayushipandey221@gmail.com>
2021-08-07 10:36:43 +0530

    zoned out for a day but back to work

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,7 +1,16 @@
 import * as api from '../api';
 
-const getPosts =() =>  async (dispatch){
+const getPosts =() =>  async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+    } catch (error) {
+        
+    }
+
+
+
+
     const action = { type: 'FECTH_ALL', payload: [] }
 
-    return action;
+    dispatch(action);
 }
